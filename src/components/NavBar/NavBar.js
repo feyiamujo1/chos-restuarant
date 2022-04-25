b5362be422b9f2605202c796067b508876a2119d
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2022-04-25 18:25:53 +0100

    First stage for product page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,18 @@
 import React from "react"
 import "./navbar.css"
 import { Link } from "react-router-dom"
+import mylogo from "../../images/logo.png"
 
 function NavBar(){
     return(
         <div className="navbar">
-            <div>
-                <img src="" alt="logo"/>
+            <div className="logo">
+                <img src= {mylogo} alt="logo"/>
             </div>
             <ul className="nav-links">
                 <li><Link to="/" className="text_link">Home</Link></li>
                 <li><Link to="/about" className="text_link">About</Link></li>
-                <li><Link to="/products" className="text_link">Products</Link></li>
+                <li><Link to="/product" className="text_link">Products</Link></li>
                 <li><Link to="/contact" className="text_link">Contact</Link></li>
             </ul>
         </div>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,39 @@
 import React from "react"
-function Product(){
+import "./product.css"
+
+function Product() {
     return(
         <div>
-            This is the Product Page
+            <div className="products_display">
+                
+            </div>
+            <div className="intro_content">
+                    <p className="intro_text_small">HAND-CRAFTED BY OUR CHEFS</p>
+                    <h1 className="intro_text_big">All in good taste!</h1>
+                </div>
+            <div>
+            <div className="main_menu_container">
+                <div className="menu_intro">
+                    <h1>Our Menu</h1>
+                    <p>Our restaurant features lavish buffets combining Continental, African, Indian, Arabic and Far 
+                        Eastern flavors with action stations, live cooking, theme nights, Sunday brunch and 
+                        healthy corners. Ideal for families, friends or even a business lunch.</p>
+                        <hr/>
+                </div>
+                <div className="menu_container">
+                    <div className="food_deplay">
+                        <div className="food_image_container">
+                            <img src="" alt=""/>
+                        </div>
+                        <div className="food_detail_container">
+                            <h2 className="food_name">Food Name here</h2>
+                            <p className="food_detail">Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                            <p className="food_price">&#8358;2000</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
